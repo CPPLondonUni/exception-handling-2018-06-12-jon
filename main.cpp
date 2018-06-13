ab19b8cc8a0d7b5c142a1880c1c43853b85d2b53
Jon <ejon748@gmail.com>
2018-06-13 14:33:50 +0100

    revised to include out of range error check  and unknown exception check

--- main.cpp
+++ main.cpp
@@ -3,31 +3,35 @@
 #include <unordered_map>
 
 int main() {
-	const std::unordered_map<std::string, int> studentAges{
-		{"john", 19},
-		{"peter", 24},
-		{"paul", 31},
-		{"zoltan", 54},
-		{"laurent", 90},
-		{"justin", 12},
-		{"oliver", 1337}
-	};
+    const std::unordered_map<std::string, int> studentAges{
+            {"john",    19},
+            {"peter",   24},
+            {"paul",    31},
+            {"zoltan",  54},
+            {"laurent", 90},
+            {"justin",  12},
+            {"oliver",  1337}
+    };
 
-	std::cout << "Enter a name: " << std::endl;
+    std::cout << "Enter a name: " << std::endl;
+    //this try catch block has the necessary syntax you need to fulfil the task.
+    try {
+        //This code is hopefully fixed.
+        std::string name;
+        std::cin >> name;
+        auto &age = studentAges.at(name); //oops, this will throw an exception if the name isn't found;
+        std::cout << name << " is " << age << " years old\n";
 
-	//This code is in a bit of a sorry state, fix it.
-	std::string name;
-	std::cin >> name;
-	auto& age = studentAges.at(name); //oops, this will throw an exception if the name isn't found;
-	std::cout << name << " is " << age << " years old\n";
+    } catch (const std::out_of_range &e) {
+        std::cout << "Oops! Error. " << e.what() << "\n";
+        //We only go inside here if the exception was thrown.
+        //if you're curious, e.what() returns a string that you can read.
+    }
+    catch (...) {
+        std::cout << "Oops! unknown exception!\n";
+        //We only go inside here if the exception was thrown.
+        //if you're curious, e.what() returns a string that you can read.
+    }
 
-	//this try catch block has the necessary syntax you need to fulfil the task.
-	try {
-		//do stuff here.
-	} catch (const std::out_of_range& e) {
-		//We only go inside here if the exception was thrown.
-		//if you're curious, e.what() returns a string that you can read.
-	}
-	
-	return 0;
+    return 0;
 }
